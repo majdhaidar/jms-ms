437bf207516d844d29d97f8bda03bf4b674c26f4
majdhaidar <haidarmajd@hotmail.com>
2024-05-04 23:01:00 +0300

    add a model to hold data messages that is sent through active mq server. Configure active mq embedded server to start.

--- src/main/java/com/sbm/jms/JmsServiceApplication.java
+++ src/main/java/com/sbm/jms/JmsServiceApplication.java
@@ -1,12 +1,25 @@
 package com.sbm.jms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		// start a new active mq server
+		ActiveMQServer activeMQServer = ActiveMQServers.newActiveMQServer(
+				new ConfigurationImpl()
+						.setPersistenceEnabled(false)
+						.setJournalDirectory("target/data/journal")
+						.setSecurityEnabled(false)
+						.addAcceptorConfiguration("invm", "vm://0")
+		);
+		activeMQServer.start();
+		// end of starting new active mq server
 		SpringApplication.run(JmsServiceApplication.class, args);
 	}
 
