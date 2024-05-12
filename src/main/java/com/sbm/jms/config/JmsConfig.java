fb21a6afdc3984fc4d748c0bbc2cf07e81aa3807
majdhaidar <haidarmajd@hotmail.com>
2024-05-12 13:10:16 +0300

    Implement configurations fro jms message convertors.

---
+++ src/main/java/com/sbm/jms/config/JmsConfig.java
@@ -0,0 +1,24 @@
+package com.sbm.jms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Class to hold all related jms configurations
+ * Including the message convertor which is used for jms messaging
+ *
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter jmsMessageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
